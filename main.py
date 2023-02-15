1dda990df225c49be20d0faf6dd48ca0d7a30377
Jocelyn Medina <113551944+jomedina17@users.noreply.github.com>
2023-02-15 20:47:18 +0000

    file

--- main.py
+++ main.py
@@ -1,2 +1,60 @@
 # remember to fork this and push to github
 
+#f = open ('test.txt', 'r')
+#print(f.readlines())
+#f.close()
+
+#with open('test.txt', 'r') as f:
+  #f_contents =f.readlines()
+  #print(f_contents, end=)
+#employee_file = open("employees.txt", "r")
+#for employee in employee_file.readlines():
+ # print(employee)
+  #print(employee_file.readable())
+#employee_file.close()
+
+#print(employee_file.read())
+#print(employee_file.readline)
+
+employee_file = open("employees.txt", "a")
+employee_file.write("Toby - Human Resources")
+employee_file.close()
+
+with open('readme.txt', 'w') as f:
+  f.write('readme')
+  #this overwrites the original content
+with open('readme.txt', 'w') as f:
+  f.write
+  
+#this appends to the original content 
+
+with open('readme.txt', 'w') as f:
+ 
+
+
+#create a file through python code 
+#get it to accept user input
+#to append the content
+
+
+with open('file.txt', 'a') as f:
+  f.write("what day is it today?")
+
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
